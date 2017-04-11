3de5d911ec9cbf4ce475e1adef45e273758903b8
Антон Пегов <apegov@kld.neolant>
2017-04-11 12:22:51 +0200

    Создан один ОБЗ, генерирующий импульсы. На него подписались две функции-наблюдателя, меняющие цвет диодов.

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 $(() => {
     const $greenLed = $('#greenLed');
     const $redLed = $('#redLed');
+
+    // Создаем ОБЗ, генерирующее импульсы раз в пол секунды
+    // share() говорит ОБЗ, что не нужно создавать новый источник 
+    // при каждой подписке, а можно использовать существующий
+    const interval$ = Rx.Observable.interval(500).share();
+
+    interval$.subscribe((c) => {
+       $greenLed.toggleClass('on'); 
+    });
+    interval$.subscribe((c) => {
+       $redLed.toggleClass('on'); 
+    });
 });
