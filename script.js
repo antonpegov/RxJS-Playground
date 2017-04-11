5373f0c3a7580677a8510383da7a0783d8f7edc6
Антон Пегов <apegov@kld.neolant>
2017-04-11 15:23:23 +0200

    Часть 2. "Живой поиск". Добавлено поле для ввода данных и стрим для него.

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 $(() => {
     
+    const PERIOD = 3000; 
     const $greenLed = $('#greenLed');
     const $redLed = $('#redLed');
     const $startBtn = $('#startBtn');
     const $stopBtn = $('#stopBtn');
+    const $search = $('#search');
+    const $list = $('#list');
 
     // ОБЗ генерирующее импульсы раз в пол секунды
     // share() говорит ОБЗ, что не нужно создавать новый источник 
     // при каждой подписке, а можно использовать существующий
-    const interval$ = Rx.Observable.interval(500).share();
+    const interval$ = Rx.Observable.interval(PERIOD).share();
 
     // Стримы (ОБЗ) для нажатий на кнопки включения и выключения
     const startClicks$ = Rx.Observable.fromEvent($startBtn, 'click');
     const stopClicks$ = Rx.Observable.fromEvent($stopBtn, 'click');
 
+    // Стрим данных от поля ввода имени пользователя
+    const input$ = Rx.Observable.fromEvent($search, 'input');
+
     startClicks$.subscribe(()=>console.log('Start Clicked')); // тест стрима нажатий на Старт
     stopClicks$.subscribe(()=>console.log('Stop Clicked'));   // тест стрима нажатий на Стоп
+    input$.map((e)=>e.target.value).subscribe((data)=>console.log(data));   // тест Инпута
 
     // Запуск и остановка мигания зеленого диода
     startClicks$
@@ -25,10 +32,15 @@ $(() => {
         .subscribe((c) => {
             $greenLed.toggleClass('on'); 
         });  
+
     // Красный диод привязан к стриму intrval$ с задержкой в 20мс    
     interval$
-        .delay(200)
+        .delay(PERIOD/2)
         .subscribe((c) => {
             $redLed.toggleClass('on'); 
         });
+
+    function getUsers(text){
+        return fetch('https://api.gihub.com/users' + text).then(res => res.json())
+    }
 });
