26940f3e016d46ecf30ab991230ce5835a367cd3
Антон Пегов <apegov@kld.neolant>
2017-04-11 12:50:39 +0200

    Добавлены две кнопки, для них созданы ОБЗ, генерирующие поток данных о нажатиях

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 $(() => {
+    
     const $greenLed = $('#greenLed');
     const $redLed = $('#redLed');
+    const $startBtn = $('#startBtn');
+    const $stopBtn = $('#stopBtn');
 
-    // Создаем ОБЗ, генерирующее импульсы раз в пол секунды
+    // ОБЗ генерирующее импульсы раз в пол секунды
     // share() говорит ОБЗ, что не нужно создавать новый источник 
     // при каждой подписке, а можно использовать существующий
     const interval$ = Rx.Observable.interval(500).share();
 
-    interval$.subscribe((c) => {
-       $greenLed.toggleClass('on'); 
-    });
+    // Стримы (ОБЗ) для нажатий на кнопки включения и выключения
+    const startClicks$ = Rx.Observable.fromEvent($startBtn, 'click');
+    const stopClicks$ = Rx.Observable.fromEvent($stopBtn, 'click');
+
+    startClicks$.subscribe(()=>console.log('Start Clicked')); // тест стрима нажатий на Старт
+    stopClicks$.subscribe(()=>console.log('Stop Clicked'));   // тест стрима нажатий на Стоп
+
+    interval$
+        .subscribe((c) => {
+            $greenLed.toggleClass('on'); 
+        });
     interval$
         .delay(200)
         .subscribe((c) => {
